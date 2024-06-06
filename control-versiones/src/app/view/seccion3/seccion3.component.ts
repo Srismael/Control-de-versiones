cf72a9f5939aac3c52b5853e5b444b64dd36e0a6
Ismael <ismaelmtzballin@gmail.com>
2024-06-05 20:07:07 -0600

    Agregado seccion3

---
+++ control-versiones/src/app/view/seccion3/seccion3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion3',
+  standalone: true,
+  imports: [],
+  templateUrl: './seccion3.component.html',
+  styleUrl: './seccion3.component.css'
+})
+export class Seccion3Component {
+
+}
