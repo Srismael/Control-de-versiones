22e9432f59bd2a42c7f7f74fa7d65af6ce585b51
Alvaro <rdzduran23@gmail.com>
2024-06-05 20:42:30 -0600

    Agregado de seccion5

---
+++ control-versiones/src/app/view/seccion5/seccion5.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion5',
+  standalone: true,
+  imports: [],
+  templateUrl: './seccion5.component.html',
+  styleUrl: './seccion5.component.css'
+})
+export class Seccion5Component {
+
+}
